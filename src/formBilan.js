44622c67dfc45c9b11d14a36731095cc774828c8
Sampicc <41238392+Sampicc@users.noreply.github.com>
2020-08-20 00:47:40 +0200

    form dom (#1)
    * Create form.txt
    
    * start form css
    
    * Fix accessibility & form
    
    * Fix invalidated base
    
    Co-authored-by: Jordan-T <jordan.taisne@gmail.fr>

---
+++ src/formBilan.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('form.formBilan');
+
+if (form.length) {
+  const select = document.getElementById('bilan_pb');
+  const sections = form.querySelectorAll('.bilan_pb');
+  const HIDDEN_CLASS = 'd-none';
+  const VALIDATED_CLASS = 'is-validated';
+
+  const onChange = (e) => {
+    const sectionNumber = e.target.value;
+    const sectionClass = `bilan_pb_${sectionNumber}`;
+
+    sections.forEach((section) => {
+      if (section.classList.contains(sectionClass)) {
+        section.classList.remove(HIDDEN_CLASS);
+      } else {
+        section.classList.add(HIDDEN_CLASS);
+      }
+    });
+  };
+
+  let tempFirstError;
+  const onInvalid = () => {
+    if (tempFirstError === undefined) {
+      form.classList.add(VALIDATED_CLASS)
+      tempFirstError = form.querySelector('input:invalid,select:invalid');
+
+      const rect = tempFirstError.getBoundingClientRect();
+      const currentTop = window.scrollY;
+      const top = rect.top - 100 + currentTop;
+
+      requestAnimationFrame(() => {
+        window.scrollTo(0, currentTop);
+        requestAnimationFrame(() => {
+          window.scrollTo({ top, left: 0, behavior: 'smooth' });
+          tempFirstError.focus();
+          tempFirstError = undefined;
+        });
+      });
+    }
+  };
+
+  select.addEventListener('change', onChange);
+  form.addEventListener('invalid', onInvalid, true);
+}
